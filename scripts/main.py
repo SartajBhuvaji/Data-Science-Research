2477b81a34a0d578287fc07e1da3108884eb12a8
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2023-08-15 14:02:39 +0530

    Added custom autoencoder

--- scripts/main.py
+++ scripts/main.py
@@ -1,25 +1,9 @@
-import pandas as pd
 import os
 import scripts.autoencoder_model as autoencoder_model
 # import your function from scripts. here #Do not remove this line, add your code above
 
-def function(file, minority_class, minority_class_column, algorithm):
+def function(input_df, minority_class, minority_class_column, algorithm, **kwargs):
 
-    autoencoders = ['autoencoder_singleencoder', 'autoencoder_balanced', 'autoencoder_heavydecoder']
-    if algorithm in autoencoders:
-        print("Autoencoder")
-        model_name = algorithm
-
-        print("Calling generate_synthetic_data")
-        synthetic_df = autoencoder_model.generate_synthetic_data(model_name=model_name,original_df = file, 
-                                minority_class_column = minority_class_column,
-                                minority_class_label = minority_class, 
-                                decoder_activation = 'sigmoid', epochs = 200)
-
-    #elif:
-        #add your algorithm here (Do not remove this line add your elif statement above this line)
-
-    #Dont change code below this line, create a synthetic_df which would be returned
     path = os.path.join(os.getcwd(), 'static', 'output')
     for filename in os.listdir(path):
         file_path = os.path.join(path, filename)
@@ -28,9 +12,59 @@ def function(file, minority_class, minority_class_column, algorithm):
                 os.unlink(file_path)
         except Exception as e:
             print(f"Error deleting file: {e}")
+    #Dont change code above this line, create a synthetic_df which would be returned
+
+
+    autoencoders = ['autoencoder_singleencoder', 'autoencoder_balanced', 'autoencoder_heavydecoder']
+    if algorithm in autoencoders:
+        try:
+            print("Autoencoder")
+            model_name = algorithm
+
+            print("Calling generate_synthetic_data")
+            synthetic_df = autoencoder_model.generate_synthetic_data(model_name=model_name,original_df = input_df, 
+                                    minority_class_column = minority_class_column,
+                                    minority_class_label = minority_class, 
+                                    decoder_activation = 'sigmoid', epochs = 200)
+        except Exception as e:
+            print(f"Error in autoencoder: {e}")
+            return 0    
+        
+    elif algorithm == "custom_autoencoder":
+        try:
+            print("Custom Autoencoder")
+            model_name = "custom_autoencoder"
+            encoder_dense_layers_str = kwargs.get('encoder_dense_layers')
+            encoder_dense_layers = encoder_dense_layers_str.split(",")
+            bottle_neck_str = kwargs.get('bottle_neck')
+            bottle_neck = int(bottle_neck_str)
+            decoder_dense_layers_str = kwargs.get('decoder_dense_layers')
+            decoder_dense_layers = decoder_dense_layers_str.split(",")
+            decoder_activation = kwargs.get('decoder_activation') 
+            epochs_str = kwargs.get('epochs')
+            epochs = int(epochs_str)
+
+            synthetic_df = autoencoder_model.generate_synthetic_data(model_name=model_name,original_df = input_df, 
+                                    minority_class_column = minority_class_column, minority_class_label = minority_class,
+                                    decoder_activation = decoder_activation, epochs = epochs,
+
+                                    encoder_dense_layers=encoder_dense_layers,
+                                    bottle_neck = bottle_neck, decoder_dense_layers = decoder_dense_layers)
+            
+        except Exception as e:
+            print(f"Error in autoencoder: {e}")
+            return 0    
+
+    '''
+    elif:
+        try: 
+        add your algorithm here (Do not remove this line add your elif statement above this line)
+
+        except Exception as e:
+            print(f"Error in your algorithm: {e}")
+            return 0
+    '''
+    #Dont change code below this line, create a synthetic_df which would be returned
 
     synthetic_df.to_csv(os.path.join(path, 'synthetic_data.csv'), index=False)
     return 1                       
-
-def testfunctionn():
-    print("IN MAIN.py")
